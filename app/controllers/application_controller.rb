3b4b24a942e6b21b981cf37e34adc0d98bd4e715
Catheryn Small <catheryn.small@gmail.com>
2020-08-18 23:41:50 -0400

    added sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "#{Sysrandom.hex(64)}"
   end
 
   get "/" do
@@ -12,7 +14,15 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    binding.pry
+    # binding.pry
+    user = User.find_by(username: params[:user][:username])
+    
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect "/account/#{user.id}"
+    else
+      redirect '/'
+    end
   end
 
   get '/signup' do
@@ -20,7 +30,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/signup' do
-    binding.pry
+    User.create(name: params[:user][:name], username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
+    redirect '/'
+  end
+
+  get '/account/:id' do
+    @user = User.find_by(params[:id])
     erb :account
   end
+
 end
--- config.ru
+++ config.ru
@@ -1,4 +1,6 @@
 require './config/environment'
+require_relative './app/models/user.rb'
+require 'securerandom'
 
 if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
