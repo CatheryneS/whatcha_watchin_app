0ded54376de97c493def178baab79f8454f5ba78
Catheryn Small <catheryn.small@gmail.com>
2020-08-21 01:37:43 -0400

    anime now belong to user

--- app/controllers/anime_controller.rb
+++ app/controllers/anime_controller.rb
@@ -3,13 +3,20 @@ class AnimeController < ApplicationController
         erb :new
     end
 
+    get '/animes/:id' do 
+        binding.pry
+        @anime = Anime.find_by(id: params[:id])
+        erb :show
+    end
+
     get '/animes' do
         @animes = Anime.all
+        @user = current_user
         erb :account
     end
 
     post '/animes' do 
-        binding.pry
-        erb :account
+        Anime.create(title: params[:anime][:title], status: params[:anime][:status], rating: params[:anime][:rating], user_id: current_user.id)
+        redirect '/animes'
     end
 end
